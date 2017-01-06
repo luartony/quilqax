f5157d16c5c27a5a0759eb14635247cc2952fabc
Luigi Arroyo <luartony@gmail.com>
2017-01-06 15:15:50 -0500

    mm

--- template/assets/js/custom.js
+++ template/assets/js/custom.js
@@ -43,14 +43,14 @@ VAGAS SLIDESHOW SCRIPTS
 ======================================*/
 $.vegas('slideshow', {
 backgrounds: [
-{ src: 'assets/img/dp1.jpg', fade: 1000, delay: 9000 },
-{ src: 'assets/img/dp2.jpg', fade: 1000, delay: 9000 },
+{ src: 'template/assets/img/dp1.jpg', fade: 1000, delay: 9000 },
+{ src: 'template/assets/img/dp2.jpg', fade: 1000, delay: 9000 },
 /*{ src: 'assets/img/1.jpg', fade: 1000, delay: 9000 },
 { src: 'assets/img/2.jpg', fade: 1000, delay: 9000 },*/
 ]
 })('overlay', {
 /** SLIDESHOW OVERLAY IMAGE **/
-src: 'assets/js/vegas/overlays/06.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
+src: 'template/assets/js/vegas/overlays/06.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
 });
 
 
